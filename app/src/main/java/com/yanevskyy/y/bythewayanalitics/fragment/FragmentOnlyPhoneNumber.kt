0add6fb06652982a561088b63293d8913644d64e
urroboros <gu.oleg1@gmail.com>
2018-02-24 16:04:36 +0200

    fix filter users with only phones numbers

--- app/src/main/java/com/yanevskyy/y/bythewayanalitics/fragment/FragmentOnlyPhoneNumber.kt
+++ app/src/main/java/com/yanevskyy/y/bythewayanalitics/fragment/FragmentOnlyPhoneNumber.kt
@@ -8,7 +8,6 @@ import android.view.ViewGroup
 import com.yanevskyy.y.bythewayanalitics.App
 import com.yanevskyy.y.bythewayanalitics.AppPresenter
 import com.yanevskyy.y.bythewayanalitics.R
-import kotlinx.android.synthetic.main.fragment_add_info.*
 import kotlinx.android.synthetic.main.fragment_only_phone_number.*
 
 class FragmentOnlyPhoneNumber : Fragment() {
@@ -21,15 +20,18 @@ class FragmentOnlyPhoneNumber : Fragment() {
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        var countOnlyPhoneNumberText = 0
+        var countOnlyPhoneNumber = 0
         presenter.userDao.users
-                .filter { user -> user.email.isEmpty() && !user.email.equals("null", true) && user.phone.isNotEmpty() && !user.phone.equals("null", true) }
-                .forEach { countOnlyPhoneNumberText++ }
+                .filter { user ->
+                    (user.email.isEmpty() || user.email.equals("null", true))
+                            && user.phone.isNotEmpty() && !user.phone.equals("null", true)
+                }
+                .forEach { countOnlyPhoneNumber++ }
 
-        displayValues(countOnlyPhoneNumberText)
+        displayValues(countOnlyPhoneNumber)
     }
 
-    private fun displayValues(countActiveTrips: Int) {
-        countAddInformationText.text = StringBuilder(countAddInformationText.text).append(countActiveTrips.toString())
+    private fun displayValues(countOnlyPhoneNumber: Int) {
+        countOnlyPhoneNumberText.text = StringBuilder(countOnlyPhoneNumberText.text).append(countOnlyPhoneNumber.toString())
     }
 }
