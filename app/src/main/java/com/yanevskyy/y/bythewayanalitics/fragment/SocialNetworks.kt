40ad971bc754fd54b22e54afe2bc5dde6b8558b7
urroboros <gu.oleg1@gmail.com>
2018-03-10 23:43:29 +0200

    add fb and cs and redone calculating percents and count

--- app/src/main/java/com/yanevskyy/y/bythewayanalitics/fragment/SocialNetworks.kt
+++ app/src/main/java/com/yanevskyy/y/bythewayanalitics/fragment/SocialNetworks.kt
@@ -2,10 +2,10 @@ package com.yanevskyy.y.bythewayanalitics.fragment
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.firebase.mm.myapplication.SocialNetwork
 import com.yanevskyy.y.bythewayanalitics.App
 import com.yanevskyy.y.bythewayanalitics.AppPresenter
 import com.yanevskyy.y.bythewayanalitics.R
@@ -21,36 +21,39 @@ class SocialNetworks : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val networks = mutableMapOf<String, Int>()
+        SocialNetwork.values().forEach { network -> networks[network.name] = 0 }
         var countAnyNetworks = 0
-        var countTG = 0
-        var countVK = 0
-        var countWhatsapp = 0
-        presenter.userDao.users.filter {user ->
-            user.socialNetwork.isNotEmpty() }
+        presenter.userDao.users.filter { user -> user.socialNetwork.isNotEmpty() }
                 .forEach { user ->
-                    countAnyNetworks++
-                    user.socialNetwork["TG"]?.let { countTG++ }
-                    user.socialNetwork["VK"]?.let { countVK++ }
-                    user.socialNetwork["WHATSAPP"]?.let { countWhatsapp++ }
+                    user.socialNetwork.forEach { currentUsersNetwork ->
+                        networks[currentUsersNetwork.key]?.let { networks[currentUsersNetwork.key] = it.inc() }
+                        countAnyNetworks++
+                    }
                 }
-
-        val percentAccountsWithTG = Math.round(countTG.toDouble() / countAnyNetworks * 100).toInt()
-        val percentAccountsWithVK = Math.round(countVK.toDouble() / countAnyNetworks * 100).toInt()
-        val percentAccountsWithWhatsapp = Math.round(countWhatsapp.toDouble() / countAnyNetworks * 100).toInt()
-
-        displayValues(countAnyNetworks, percentAccountsWithTG, percentAccountsWithVK, percentAccountsWithWhatsapp,
-                countTG, countVK, countWhatsapp)
+        val percentsNetworks = mutableMapOf<String, Int>()
+        networks.forEach { currentNetwork ->
+            percentsNetworks[currentNetwork.key] = Math.round(currentNetwork.value.toDouble() / countAnyNetworks * 100).toInt()
+        }
+        displayValues(countAnyNetworks, percentsNetworks, networks)
     }
 
-    private fun displayValues(countAnyNetworks: Int, percentTG: Int, percentVK: Int, percentWhatsapp: Int, countTG: Int,
-                              countVK: Int, countWhatsapp: Int) {
+    private fun displayValues(countAnyNetworks: Int, percentsNetworks: MutableMap<String, Int>, networks: MutableMap<String, Int>) {
         countAnyNetworkText.text = StringBuilder(context?.getString(R.string.contains_any_networks)).append(" ")
                 .append(countAnyNetworks)
-        countTGText.text = StringBuilder(context?.getString(R.string.contains_telegram)).append(" ")
-                .append(countTG).append(" (").append(percentTG).append("%").append(")")
-        countVKText.text = StringBuilder(context?.getString(R.string.contains_vk)).append(" ").append(countVK)
-                .append(" (").append(percentVK).append("%").append(")")
-        countWhatsappText.text = StringBuilder(context?.getString(R.string.contains_whatsapp)).append(" ")
-                .append(countWhatsapp).append(" (").append(percentWhatsapp).append("%").append(")")
+        networks.forEach { networkCount ->
+            percentsNetworks[networkCount.key]?.let { percentsNetwork ->
+                val displayCount = when (networkCount.key) {
+                    in SocialNetwork.CS.name -> countCSText
+                    in SocialNetwork.FB.name -> countFBText
+                    in SocialNetwork.TG.name -> countTGText
+                    in SocialNetwork.VK.name -> countVKText
+                    in SocialNetwork.WHATSAPP.name -> countWhatsappText
+                    else -> null
+                }
+                displayCount?.text = StringBuilder().append(" ").append(networkCount).append(" (")
+                        .append(percentsNetwork).append("%").append(")")
+            }
+        }
     }
 }
